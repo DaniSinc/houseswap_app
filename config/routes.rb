fbbf309d8f13b36cac06e3299f111c129c04531a
Daniela Sinclair <dasfluid@gmail.com>
2020-11-22 20:57:24 +0000

    First commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/parking", to: "pages#parking"
+  get "/alarms", to: "pages#alarms"
+  get "/kitchen", to: "pages#kitchen"
+  get "/doors", to: "pages#doors"
+  get "/bathrooms", to: "pages#bathrooms"
+  get "/rubbish", to: "pages#rubbish"
+  get "/shed", to: "pages#shed"
+  get "/living_room", to: "pages#living_room"
+  get "/controls", to: "pages#controls"
 end
